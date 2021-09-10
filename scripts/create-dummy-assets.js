e091f7e298717a86b0523b97a146dce35fe4b840
Matías <mjlescano@protonmail.com>
2021-09-10 18:05:59 +0200

    add rarity to generated assets

--- scripts/create-dummy-assets.js
+++ scripts/create-dummy-assets.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const del = require('del');
+const random = require('random');
 const PNGlib = require('node-pnglib');
 const Confirm = require('prompt-confirm');
 const randomColor = require('random-color');
@@ -28,7 +29,14 @@ const createDummyAssets = async () => {
     await del([path.join(ASSETS_FOLDER, '*.png')]);
   }
 
+  const _r = random.exponential(10.1);
+  const getRarity = () => {
+    let n = _r();
+    return n > 1 ? getRarity() : n;
+  };
+
   const promises = [];
+  let rarities = [];
 
   for (let x = 0; x < TOTAL_ASSETS; x++) {
     let png = new PNGlib(150, 150);
@@ -45,7 +53,10 @@ const createDummyAssets = async () => {
     const metadata = {
       name: faker.hacker.adjective(),
       description: faker.hacker.phrase(),
+      rarity: getRarity(),
     };
+
+    rarities.push(metadata.rarity);
 
     const assetPath = path.join(ASSETS_FOLDER, `${x}.png`);
     const metadataPath = path.join(ASSETS_FOLDER, `${x}.json`);
@@ -57,8 +68,36 @@ const createDummyAssets = async () => {
   }
 
   await Promise.all(promises);
+
+  rarities = rarities.sort();
+
+  const distribution = [
+    ...rarities.reduce((d, rarity) => {
+      const i = Math.floor(rarity * 10);
+      d[i] = (d[i] || 0) + 1;
+      return d;
+    }, []),
+  ].map((v) => v || 0);
+
+  const title = ` Generated ${TOTAL_ASSETS} assets `;
+
+  console.log('');
+  console.log(title.replace(/.{1}/g, '='));
+  console.log(title);
+  console.log(title.replace(/.{1}/g, '='));
+  console.log('');
+  console.log(`Min rarity: ${rarities[0]}`);
+  console.log(`Max rarity: ${rarities[rarities.length - 1]}`);
+  console.log('');
+  console.log('Rarity distribution: ');
+
+  distribution.forEach((count, i) => {
+    console.log(`  [0.${`${i}`.padEnd(2, '0')}-0.${`${i + 1}`.padEnd(2, '0')}): ${count} assets`);
+  });
+  console.log('');
 };
 
-createDummyAssets()
-  .then(() => console.log('Dummy assets has been created!'))
-  .catch(console.error);
+createDummyAssets().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
