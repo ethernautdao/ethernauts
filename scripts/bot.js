7dc1dc0771f6ff37649ac1d7299d8e0c01c21267
Matías <mjlescano@protonmail.com>
2021-09-10 18:18:49 +0200

    fix config location

--- scripts/bot.js
+++ scripts/bot.js
@@ -3,7 +3,7 @@ const path = require('path');
 const hre = require('hardhat');
 
 const IPFS = require('../src/ipfs');
-const config = require('../src/config');
+const config = require('../config');
 
 const { ethers } = hre;
 
