a0b73c76ee4ab25547e07f2ce4e9052a451ae941
Sebastián Lorenzo <SebastianLorenzo@gmail.com>
2021-11-04 16:24:39 -0300

    Add script for signing coupons (#86)
    * add script for signing coupons
    
    * implemented suggestions
    
    * add some comments
    
    * fix variable usage

---
+++ packages/hardhat/scripts/create-signed-coupons.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const { utils } = require('ethers');
+const { ethers } = require('hardhat');
+
+const constants = require('../src/constants');
+
+const awardedAccounts = require(`../src/data/awarded-accounts.${process.env.HARDHAT_NETWORK}`);
+
+const signCouponForAddress = async (address, signer) => {
+  const payload = `0x000000000000000000000000${address.replace('0x', '')}`;
+  const payloadHash = utils.keccak256(payload);
+  const payloadHashBytes = utils.arrayify(payloadHash);
+
+  const signedCoupon = await signer.signMessage(payloadHashBytes);
+
+  return {
+    [address]: signedCoupon,
+  };
+};
+
+async function main() {
+  // TODO: Get the signer from a .env file's private key.
+  const [couponSigner] = await ethers.getSigners();
+
+  const signedCouponsPath = path.join(
+    constants.DATA_DAPP_FOLDER,
+    `signed-coupons.${process.env.HARDHAT_NETWORK}.json`
+  );
+
+  const coupons = awardedAccounts.map((account) => signCouponForAddress(account, couponSigner));
+
+  const signedCoupons = await Promise.all(coupons);
+
+  return fs.promises.writeFile(signedCouponsPath, JSON.stringify(signedCoupons, null, 2));
+}
+
+main()
+  .then(() => console.log('Signed coupons has been created successfully'))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
--- packages/hardhat/src/constants.js
+++ packages/hardhat/src/constants.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 const TOTAL_ASSETS = 10000;
 const IPFS_PREFIX = 'ipfs://';
+
+const DAPP_FOLDER = path.resolve(__dirname, '..', '..', 'dapp');
+const DATA_DAPP_FOLDER = path.join(DAPP_FOLDER, 'data');
+
 const RESOURCES_FOLDER = path.resolve(__dirname, '..', 'resources');
 const ASSETS_FOLDER = path.join(RESOURCES_FOLDER, 'assets');
 const METADATA_FOLDER = path.join(RESOURCES_FOLDER, 'metadata');
@@ -9,6 +13,8 @@ const METADATA_FOLDER = path.join(RESOURCES_FOLDER, 'metadata');
 module.exports = {
   TOTAL_ASSETS,
   IPFS_PREFIX,
+  DAPP_FOLDER,
+  DATA_DAPP_FOLDER,
   RESOURCES_FOLDER,
   ASSETS_FOLDER,
   METADATA_FOLDER,
