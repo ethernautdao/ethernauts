7a6192e924f549b8b8291b54d12ac733ec851369
slorenzo <sebastianlorenzo@gmail.com>
2021-11-16 16:32:09 -0300

    fix installed packages

--- packages/hardhat/tasks/mint.js
+++ packages/hardhat/tasks/mint.js
@@ -1,48 +1,44 @@
-const fs = require('fs');
-const fsp = require('fs/promises');
-const path = require('path');
 const { NumberPrompt } = require('enquirer');
 const { getContractAt } = require('../src/utils/hardhat');
 const { task } = require('hardhat/config');
 
 const SALE_STATES = ['Paused', 'Early', 'Open'];
 
-task('mint', 'Mints Ethernauts tokens')
-  .setAction(async (taskArguments, hre) => {
-    const Ethernauts = await getContractAt('Ethernauts');
+task('mint', 'Mints Ethernauts tokens').setAction(async (taskArguments, hre) => {
+  const Ethernauts = await getContractAt('Ethernauts');
 
-    const prompt = new NumberPrompt({
-      name: 'Number of tokens to mint',
-      message: 'How many tokens would you like to mint?',
-    });
+  const prompt = new NumberPrompt({
+    name: 'Number of tokens to mint',
+    message: 'How many tokens would you like to mint?',
+  });
 
-    const answer = await prompt.run();
-    console.log(answer);
+  const answer = await prompt.run();
+  console.log(answer);
 
-    Ethernauts.on('Transfer', async (from, to, amount, event) => {
-      if (from === '0x0000000000000000000000000000000000000000') {
-        const tokenId = event.args.tokenId.toString();
+  Ethernauts.on('Transfer', async (from, to, amount, event) => {
+    if (from === '0x0000000000000000000000000000000000000000') {
+      const tokenId = event.args.tokenId.toString();
 
-        const tx = await hre.ethers.provider.getTransaction(event.transactionHash);
-        const value = hre.ethers.utils.formatEther(tx.value);
+      const tx = await hre.ethers.provider.getTransaction(event.transactionHash);
+      const value = hre.ethers.utils.formatEther(tx.value);
 
-        console.log(`Mint of token #${tokenId} detected. User paid ${value} ETH`);
-      }
-    });
-
-    if (isNaN(answer)) {
-      throw new Error(`Invalid amount ${answer}`);
-    }
-
-    for (let i = 0; i < answer; i++) {
-      const value = Math.random() * 13.4 + 0.2;
-
-      const tx = await Ethernauts.mint({
-        value: hre.ethers.utils.parseEther(`${value}`),
-      });
-
-      await tx.wait();
-
-      console.log('Token minted!');
+      console.log(`Mint of token #${tokenId} detected. User paid ${value} ETH`);
     }
   });
+
+  if (isNaN(answer)) {
+    throw new Error(`Invalid amount ${answer}`);
+  }
+
+  for (let i = 0; i < answer; i++) {
+    const value = Math.random() * 13.4 + 0.2;
+
+    const tx = await Ethernauts.mint({
+      value: hre.ethers.utils.parseEther(`${value}`),
+    });
+
+    await tx.wait();
+
+    console.log('Token minted!');
+  }
+});
--- packages/hardhat/tasks/sale-state.js
+++ packages/hardhat/tasks/sale-state.js
@@ -1,27 +1,22 @@
-const fs = require('fs');
-const fsp = require('fs/promises');
-const path = require('path');
 const List = require('prompt-list');
 const { getContractAt } = require('../src/utils/hardhat');
 const { task } = require('hardhat/config');
 
 const SALE_STATES = ['Paused', 'Early', 'Open'];
 
-task('sale-state', 'Changes the sale state of the Ethernauts NFT contract')
-  .setAction(async (taskArguments, hre) => {
+task('sale-state', 'Changes the sale state of the Ethernauts NFT contract').setAction(
+  async (taskArguments, hre) => {
     const Ethernauts = await getContractAt('Ethernauts');
 
     const saleStateIdxToName = (idx) => SALE_STATES[idx];
     const saleStateNameToIdx = (name) => SALE_STATES.indexOf(name);
 
-    console.log(`Current sale state ${saleStateIdxToName(
-      await Ethernauts.currentSaleState()
-    )}`);
+    console.log(`Current sale state ${saleStateIdxToName(await Ethernauts.currentSaleState())}`);
 
     const list = new List({
       name: 'Sale state',
       message: 'Change sale state?',
-      choices: SALE_STATES
+      choices: SALE_STATES,
     });
 
     const answer = await list.run();
@@ -30,4 +25,5 @@ task('sale-state', 'Changes the sale state of the Ethernauts NFT contract')
     const tx = await Ethernauts.setSaleState(saleStateNameToIdx(answer));
     const receipt = await tx.wait();
     console.log(receipt);
-  });
+  }
+);
