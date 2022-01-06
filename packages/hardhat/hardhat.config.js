2abcb4c0ee6b405d55448dc6c1c4890ec76dfe9b
Mario <baisan.marius@gmail.com>
2022-01-06 17:15:46 +0200

    changed batch size to 50

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -42,7 +42,7 @@ module.exports = {
   defaults: {
     maxGiftable: 100,
     maxTokens: 10000,
-    batchSize: 500,
+    batchSize: 50,
     mintPrice: ethers.utils.parseEther('0.2'),
     earlyMintPrice: ethers.utils.parseEther('0.015'),
     initialCouponSigner: '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266', // Default hardhat signer[0]
