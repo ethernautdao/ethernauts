62e452f910da72509489eec8c3668b27966c6081
slorenzo <sebastianlorenzo@gmail.com>
2022-01-06 22:33:11 -0300

    Add some libraries

--- packages/dapp/pages/_app.js
+++ packages/dapp/pages/_app.js
@@ -3,11 +3,15 @@ import { DonationProvider } from '../contexts/DonationProvider';
 
 import { Layout } from '../components/Layout';
 
+import 'react-medium-image-zoom/dist/styles.css';
+import 'rc-slider/assets/index.css';
+
 import '../styles/globals.scss';
 import '../styles/fullpage.scss';
 import '../styles/theme.scss';
-import 'rc-slider/assets/index.css';
+
 import '../styles/rc-slider.scss';
+import '../styles/react-medium-image-zoom.scss';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
