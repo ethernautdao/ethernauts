88b85a4d02d5aa9ed3f04b2482491f45d822e7dd
koloz193 <zach.kolodny@gmail.com>
2022-01-11 10:19:27 -0500

    added natspec comments to public non-owner functions (#163)
    * added natspec comments to public non-owner functions
    
    * added natspec comments for onlyOwner functions

--- packages/hardhat/contracts/Ethernauts.sol
+++ packages/hardhat/contracts/Ethernauts.sol
@@ -112,6 +112,7 @@ contract Ethernauts is ERC721Enumerable, Ownable {
     // Public external ABI
     // --------------------
 
+    /// @notice Mints a single token if at least mintPrice is sent and there are tokens available to mint.
     function mint() external payable onlyOnState(SaleState.Open) {
         if (msg.value < mintPrice) {
             revert MintPriceError({sent: msg.value, required: mintPrice});
@@ -128,6 +129,8 @@ contract Ethernauts is ERC721Enumerable, Ownable {
         }
     }
 
+    /// @notice Allows the sender to mint while in early sale state.
+    /// @param signedCoupon Coupon given by couponSigner giving the sender early mint access.
     function mintEarly(bytes memory signedCoupon) external payable onlyOnState(SaleState.Early) {
         if (msg.value < earlyMintPrice) {
             revert EarlyMintPriceError({sent: msg.value, required: earlyMintPrice});
@@ -150,26 +153,41 @@ contract Ethernauts is ERC721Enumerable, Ownable {
         _redeemedCoupons[msg.sender] = true;
     }
 
+    /// @notice The number of tokens gifted.
+    /// @return The number of tokens gifted.
     function tokensGifted() external view returns (uint256) {
         return _tokensGifted;
     }
 
+    /// @notice Total number of tokens available.
+    /// @return The current number of available tokens (max - total current supply).
     function availableSupply() public view returns (uint256) {
         return maxTokens - totalSupply();
     }
 
+    /// @notice Total number of tokens available for minting.
+    /// @return The current number of mintable tokens (available supply - gifted supply).
     function availableToMint() public view returns (uint256) {
         return availableSupply() - availableToGift();
     }
 
+    /// @notice Remaining giftable tokens.
+    /// @return The amount of giftable tokens remaining (total giftable - already gifted).
     function availableToGift() public view returns (uint256) {
         return maxGiftable - _tokensGifted;
     }
 
+    /// @notice Checks if a token with tokenId exists.
+    /// @param tokenId The Id being checked.
+    /// @return true if token exists
     function exists(uint256 tokenId) external view returns (bool) {
         return _exists(tokenId);
     }
 
+    /// @notice Checks if the supplied coupon is for the given user.
+    /// @param user Address of user.
+    /// @param coupon Coupon by couponSigner of the user's address.
+    /// @return Returns true if the couponSigner signed for supplied user.
     function isCouponSignedForUser(address user, bytes memory coupon) public view returns (bool) {
         bytes32 messageHash = keccak256(abi.encode(user));
         bytes32 prefixedHash = ECDSA.toEthSignedMessageHash(messageHash);
@@ -179,10 +197,16 @@ contract Ethernauts is ERC721Enumerable, Ownable {
         return couponSigner == retrievedSigner;
     }
 
+    /// @notice Checks to see if the user has redeemed a coupon.
+    /// @param user Address of the user.
+    /// @return True is the user has redeemed a coupon.
     function userRedeemedCoupon(address user) public view returns (bool) {
         return _redeemedCoupons[user];
     }
 
+    /// @notice Returns the uri for a given token.
+    /// @param tokenId Id of token
+    /// @return URI of `tokenId` token
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         string memory baseURI = _baseURI();
 
@@ -203,10 +227,15 @@ contract Ethernauts is ERC721Enumerable, Ownable {
         return string(abi.encodePacked(baseURI, assetId.toString()));
     }
 
+    /// @notice Fetch the random number for `batchId`
+    /// @param batchId Id for the batch.
+    /// @return Random number for batchId
     function getRandomNumberForBatch(uint batchId) public view returns (uint) {
         return _randomNumbers[batchId];
     }
 
+    /// @notice Get the number of random numbers
+    /// @return Number of random numbers in `_randomNumbers`
     function getRandomNumberCount() public view returns (uint) {
         return _randomNumbers.length;
     }
@@ -215,6 +244,9 @@ contract Ethernauts is ERC721Enumerable, Ownable {
     // Protected external ABI
     // -----------------------
 
+    /// @notice Gifts a token to the `to` address.
+    /// @dev This can only be called by the contract owner.
+    /// @param to The address the token is being gifted to.
     function gift(address to) external onlyOwner {
         if (_tokensGifted >= maxGiftable) {
             revert TokensGiftError({gifted: _tokensGifted, maxToGift: maxGiftable});
@@ -225,16 +257,25 @@ contract Ethernauts is ERC721Enumerable, Ownable {
         _tokensGifted += 1;
     }
 
+    /// @notice Sets the new mint price for a token.
+    /// @dev This can only be called by the contract owner.
+    /// @param newMintPrice The new price a token can be bought for.
     function setMintPrice(uint256 newMintPrice) external onlyOwner {
         mintPrice = newMintPrice;
         emit MintPriceChanged(newMintPrice);
     }
 
+    /// @notice Sets the new early mint price for a token.
+    /// @dev This can only be called by the contract owner.
+    /// @param newEarlyMintPrice The new early price a token can be bought for.
     function setEarlyMintPrice(uint256 newEarlyMintPrice) external onlyOwner {
         earlyMintPrice = newEarlyMintPrice;
         emit EarlyMintPriceChanged(newEarlyMintPrice);
     }
 
+    /// @notice Sets the base URI for all token URIs.
+    /// @dev This can only be called by the contract owner. Can only be called if NFTs arent done minting.
+    /// @param newBaseTokenURI The new base URI for tokens.
     function setBaseURI(string calldata newBaseTokenURI) external {
         if (msg.sender != owner() && msg.sender != urlChanger) revert NotAuthorized(msg.sender);
         if (permanentUrl) {
@@ -245,6 +286,9 @@ contract Ethernauts is ERC721Enumerable, Ownable {
         emit BaseTokenURIChanged(newBaseTokenURI);
     }
 
+    /// @notice Set the sale state for tokens.
+    /// @dev This can only be called by the contract owner.
+    /// @param newSaleState The new sale state of the tokens.
     function setSaleState(SaleState newSaleState) external onlyOwner {
         if (currentSaleState == SaleState.PublicCompleted) {
             revert CurrentStateError({currentState: currentSaleState, availableState: SaleState.PublicCompleted});
@@ -262,26 +306,42 @@ contract Ethernauts is ERC721Enumerable, Ownable {
         emit SaleStateChanged(newSaleState);
     }
 
+    /// @notice Set the address that can issue coupons.
+    /// @dev This can only be called by the contract owner.
+    /// @param newCouponSigner New address that can issue coupons.
     function setCouponSigner(address newCouponSigner) external onlyOwner {
         couponSigner = newCouponSigner;
         emit CouponSignerChanged(newCouponSigner);
     }
 
+    /// @notice Freeze the URI so that it cant be updated anymore.
+    /// @dev This can only be called by the contract owner.
     function setPermanentURI() external onlyOwner {
         permanentUrl = true;
         emit PermanentURITriggered(true);
     }
 
+    /// @notice Sets address of `urlChanger`
+    /// @dev This can only be called by the contract owner.
+    /// @param newUrlChanger New address that can change the URI
     function setUrlChanger(address newUrlChanger) external onlyOwner {
         urlChanger = newUrlChanger;
         emit UrlChangerChanged(newUrlChanger);
     }
 
+    /// @notice Withdraws all eth currently held by the contract.
+    /// @dev This can only be called by the contract owner. sendValue used to avoid 2300 gas issuance complications.
+    /// @param beneficiary The address that funds will be withdrawn to.
     function withdraw(address payable beneficiary) external onlyOwner {
         beneficiary.sendValue(address(this).balance);
         emit WithdrawTriggered(beneficiary);
     }
 
+    /// @notice Withdraw `value` tokens held by this contract.
+    /// @dev This can only be called by the contract owner.
+    /// @param token Contract address of erc20 token.
+    /// @param to Address tokens are being sent to.
+    /// @param value The amount of tokens being withdrawn.
     function recoverTokens(
         address token,
         address to,
