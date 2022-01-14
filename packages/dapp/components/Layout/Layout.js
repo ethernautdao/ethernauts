52976e0ae05c7976a5f4a235c06f5308f6dea151
saeta.eth <sebastianlorenzo@gmail.com>
2022-01-14 10:08:22 -0300

    Switch network depending on the environment

--- packages/dapp/components/Layout/Layout.js
+++ packages/dapp/components/Layout/Layout.js
@@ -1,14 +1,20 @@
 import cn from 'classnames';
 
+import useSwitchNetwork from '../../hooks/useSwitchNetwork';
+
 import { Background } from '../Background';
 
 import styles from './Layout.module.scss';
 
-const Layout = ({ children }) => (
-  <>
-    <Background />
-    <main className={cn(styles.main)}>{children}</main>
-  </>
-);
+const Layout = ({ children }) => {
+  useSwitchNetwork();
+
+  return (
+    <>
+      <Background />
+      <main className={cn(styles.main)}>{children}</main>
+    </>
+  );
+};
 
 export default Layout;
---
+++ packages/dapp/hooks/useSwitchNetwork.js
@@ -0,0 +1,39 @@
+import { utils, BigNumber } from 'ethers';
+import { useContext, useEffect } from 'react';
+
+import { WalletContext } from '../contexts/WalletProvider';
+import { OPTIMISM_NETWORKS, DEFAULT_NETWORKS_PER_ENVIRONMENT } from '../constants/networks';
+
+const useSwitchNetwork = () => {
+  const { state } = useContext(WalletContext);
+
+  const { web3Provider, address, chainId } = state;
+
+  useEffect(async () => {
+    if (!web3Provider || !chainId || !address) return;
+
+    if (!web3Provider.provider || !web3Provider.provider.request) return;
+
+    const defaultChainId = DEFAULT_NETWORKS_PER_ENVIRONMENT[process.env.NODE_ENV];
+
+    if (chainId === defaultChainId) return;
+
+    const formattedChainId = utils.hexStripZeros(BigNumber.from(defaultChainId).toHexString());
+
+    try {
+      await web3Provider.provider.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: formattedChainId }],
+      });
+    } catch (e) {
+      if (e?.message?.includes('Unrecognized chain ID')) {
+        await web3Provider.provider.request({
+          method: 'wallet_addEthereumChain',
+          params: [OPTIMISM_NETWORKS[formattedChainId]],
+        });
+      }
+    }
+  }, [web3Provider, chainId, address]);
+};
+
+export default useSwitchNetwork;
