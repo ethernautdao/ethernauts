847160b0a7565faff2fab140f94457b96d1e4f1b
saeta.eth <sebastianlorenzo@gmail.com>
2022-01-30 20:37:21 -0300

    Add footer/wallet connect to layout

--- packages/dapp/components/Footer/Footer.js
+++ packages/dapp/components/Footer/Footer.js
@@ -4,20 +4,21 @@ import { Twitter } from '../Icons/Twitter';
 import { Discord } from '../Icons/Discord';
 import { Medium } from '../Icons/Medium';
 
-const Footer = () => {
-
-  return (
-    <>
-      <div className={(styles.footer)}>
-        <div className={(styles.social)}>
-          <Twitter />
-          <Discord />
-          <Medium />
-        </div>
-        <p>EthernautDAO 2022 ✧</p>
-      </div>
-    </>
-  );
-};
+const Footer = () => (
+  <div className={styles.footer}>
+    <div className={styles.social}>
+      <a href="https://twitter.com/EthernautDAO" target="_blank">
+        <Twitter />
+      </a>
+      <a href="https://discord.gg/RQ5WYDxUF3" target="_blank">
+        <Discord />
+      </a>
+      <a href="https://ethernautdao.medium.com/" target="_blank">
+        <Medium />
+      </a>
+    </div>
+    <p>EthernautDAO 2022 ✧</p>
+  </div>
+);
 
 export default Footer;
--- packages/dapp/components/WalletInfo/WalletInfo.js
+++ packages/dapp/components/WalletInfo/WalletInfo.js
@@ -1,4 +1,5 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import { utils } from 'ethers';
 
 import { WalletContext } from '../../contexts/WalletProvider';
 import ellipseAddress from '../../helpers/ellipse-address';
@@ -9,7 +10,8 @@ import { Dot } from './Dot';
 import styles from './WalletInfo.module.scss';
 
 const WalletInfo = () => {
-  const { state } = useContext(WalletContext);
+  const { state, disconnect } = useContext(WalletContext);
+  const [isHover, setIsHover] = useState(false);
 
   const { address, chainId } = state;
 
@@ -23,19 +25,26 @@ const WalletInfo = () => {
   if (!state.address) return null;
 
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
       <div className={styles.walletInfo}>
-        <div className={styles.balance}>12.345 ETH</div>
+        <div className={styles.balance}>
+          {`${Number(utils.formatEther(state.balance.toString())).toFixed(2)}`} ETH
+        </div>
         <div className={styles.address}>
           <Dot />
           <div>{ellipseAddress(address)}</div>
         </div>
       </div>
-      {/* Show this only when clicking / hovering the 'walletInfo' component */}
-      <a className={styles.disconnect}>
-        <img src={'/assets/ic-disconnect.svg'} />
-        <span>Disconnect wallet</span>
-      </a>
+      {isHover && (
+        <a className={styles.disconnect} onClick={disconnect}>
+          <img src={'/assets/ic-disconnect.svg'} />
+          <span>Disconnect wallet</span>
+        </a>
+      )}
     </div>
   );
 };
