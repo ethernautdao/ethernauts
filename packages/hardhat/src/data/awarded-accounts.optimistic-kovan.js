f32d432490589a21963c9e7c198758b082ec5593
saeta.eth <SebastianLorenzo@gmail.com>
2022-02-04 12:08:36 -0300

    Add elegible addresses for coupons (#250)
    * Add elegible addresses for coupons
    
    * add deployer to the awarded accounts

--- packages/hardhat/src/data/awarded-accounts.optimistic-kovan.js
+++ packages/hardhat/src/data/awarded-accounts.optimistic-kovan.js
@@ -1,8 +1,3 @@
-/**
- * The following addresses are hardhat accounts
- * We use them to create signed coupons for development reasons.
- */
-
 const accounts = [
   '0x6c68b892bF43AaD817652DB4B6daB8Ac5Ed3a7F5',
   '0xA227D2b83aB33384124A75872e1f3D71b6341D0b',
