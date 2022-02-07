b59388b7dae6f48225db81eaddea6a2514b23764
saeta.eth <SebastianLorenzo@gmail.com>
2022-02-07 10:53:57 -0300

    Fix early coupon for non-formatted addresses (#258)
    * Fix early coupon for non-formatted address
    
    * fix
    
    * fix

--- packages/dapp/hooks/useEarlyMint.js
+++ packages/dapp/hooks/useEarlyMint.js
@@ -31,7 +31,7 @@ const useMintEarly = () => {
 
         const signedCoupon = signedCoupons.find((signedCoupon) => {
           const [address] = Object.keys(signedCoupon);
-          return address === state.address;
+          return address.toLowerCase() === state.address.toLowerCase();
         });
 
         if (!signedCoupon)
@@ -43,7 +43,9 @@ const useMintEarly = () => {
 
         if (isARedeemedCoupon) throw new Error(`You've redeemed your community sale coupon`);
 
-        const tx = await contract.mintEarly(signedCoupon[state.address], {
+        const [coupon] = Object.values(signedCoupon);
+
+        const tx = await contract.mintEarly(coupon, {
           value: utils.parseEther(String(donation)),
         });
 
