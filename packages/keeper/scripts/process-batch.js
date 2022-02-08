bd4641ed90dc6d098493d3bb54db79a38e89b38e
Matías Lescano <mjlescano@users.noreply.github.com>
2022-02-08 11:48:50 -0300

    fix asset filename allocation (#260)

---
+++ packages/keeper/scripts/process-batch.js
@@ -0,0 +1,45 @@
+require('../src/errors-catch');
+
+const { FlowProducer } = require('bullmq');
+const { getContractFromAbi } = require('@ethernauts/hardhat/src/utils/hardhat');
+const config = require('../src/config');
+const { JOB_PROCESS_BATCH } = require('../src/constants');
+
+const queue = new FlowProducer({
+  connection: {
+    host: config.REDIS_HOST,
+    port: config.REDIS_PORT,
+  },
+});
+
+async function main() {
+  const batchNumber = Number(process.argv[2]);
+
+  const Ethernauts = await getContractFromAbi('Ethernauts');
+
+  console.log(' - Enqueueing Re Process Batch Job -');
+  console.log(`   Address: ${Ethernauts.address}`);
+
+  const batchSize = Number(await Ethernauts.batchSize());
+  const maxTokens = Number(await Ethernauts.maxTokens());
+  const maxBatchNumber = Math.floor(maxTokens / batchSize) - 1;
+
+  if (!/^[0-9]+$/.test(batchNumber) || batchNumber > maxBatchNumber) {
+    throw new Error(`Invalid batch number ${batchNumber}`);
+  }
+
+  console.log(`   maxTokens: ${batchSize} `);
+  console.log(`   BatchSize: ${batchSize} `);
+  console.log(`   maxBatchNumber: ${batchSize} `);
+
+  await queue.add({
+    name: JOB_PROCESS_BATCH,
+    queueName: config.MINTS_QUEUE_NAME,
+    data: {
+      batchNumber,
+      batchSize,
+    },
+  });
+}
+
+main();
