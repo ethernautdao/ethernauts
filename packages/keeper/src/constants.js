5c93552c1365692fb5d417d8c8bc1a8bc31306a3
Matías Lescano <mjlescano@users.noreply.github.com>
2022-02-15 13:21:37 -0300

    after a batch ends, check several times for baseURI update (#266)

--- packages/keeper/src/constants.js
+++ packages/keeper/src/constants.js
@@ -1,4 +1,5 @@
 module.exports = {
+  JOB_PROCESS_BATCH_END: 'job-process-batch-end',
   JOB_PROCESS_BATCH: 'job-batch-end',
   JOB_UPDATE_BASE_URL: 'job-batch-url-update',
   JOB_UPLOAD_RESOURCE: 'job-upload-resource',
